9f88779b22f5402a606e6d52eb56c508f362c5c4
roma <rommur5@mail.ru>
2023-05-01 18:41:27 +0300

    1-3 задание

--- src/java/ru/roman/Main.java
+++ src/java/ru/roman/Main.java
@@ -3,9 +3,20 @@ package ru.roman;
 public class Main {
     public static void main(String[] args) {
 
-        task1();
+//        task1_2();
+        task3();
     }
-    private static void task1() {
-
+    private static void task1_2() {
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String astName = "Ivanov";
+        String fullName = astName+" "+firstName+" "+middleName;
+        System.out.println("ФИО сотрудника- "+fullName);
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — "+fullName.toUpperCase());
+    }
+    private static void task3(){
+        String fullName="Иванов Семён Семёнович";
+        fullName=fullName.replace("ё","е");
+        System.out.println("Данные ФИО сотрудника — "+fullName);
     }
 }
